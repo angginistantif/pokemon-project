d317a24838627d64823598b5ac1924f7b7f2381a
angginistantif <angginistanti.fairuz@ui.ac.id>
2021-09-22 13:24:26 +0700

    modified capitalize letter

--- pages/[pageName]/index.tsx
+++ pages/[pageName]/index.tsx
@@ -73,7 +73,7 @@ class Home extends React.Component <Props, State> {
           <Breadcrumb.Divider />
           <Breadcrumb.Section active>{router.query.pageName}</Breadcrumb.Section>
         </Breadcrumb>
-        <Navbar title={router.query.pageName?.toString().charAt(0).toUpperCase().concat(router.query.pageName?.toString().slice(1).toLowerCase())}/>
+        <Navbar title={router.query.pageName}/>
         <br/><br/>
 
         <Card.Group centered textAlign='center' >
--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = ({ title }: Props) => {
   return (
     <Container textAlign='center'>
     <Header as='h1'>Welcome to Pokémon API</Header>
-    <Header.Subheader>{title}</Header.Subheader>
+    <Header.Subheader>{title?.toString().charAt(0).toUpperCase().concat(title?.toString().slice(1).toLowerCase())}</Header.Subheader>
     </Container>
   );
 };
