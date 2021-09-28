2ec790e5c70b8f6e86f2a511d40fa4ad2a5ff776
angginistantif <angginistanti.fairuz@ui.ac.id>
2021-09-28 07:43:30 +0700

    modifief query

--- pages/[pageName]/index.tsx
+++ pages/[pageName]/index.tsx
@@ -1,44 +1,65 @@
 import Head from 'next/head'
 import React from 'react'
 import axios from 'axios'
-import { Image, Card, Header, Container, Grid, Breadcrumb } from 'semantic-ui-react';
+import { Image, Card, Header, Container, Grid, Breadcrumb, Form, Button } from 'semantic-ui-react';
 import Link from 'next/link'
 import Error from '../_error'
 import router from 'next/router'
 
 interface Props {
+  query?: [] | any;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   
 }
 interface State {
-    homeData?: Array<any>;
+    pageType?: string;
     pageData ?: Array<any>;
     isReady?: boolean;
     isError?: boolean;
     statusCode?: number;
+    totalFetch?: number;
 }
 
 class Home extends React.Component <Props, State> {
 
   constructor (props : Props){
     super(props);
+    this.changeFetch = this.changeFetch.bind(this)
+    this.getData = this.getData.bind(this)
     this.state = {
       isReady: false,
       pageData : [],
-      homeData : [],
+      pageType : '',
       isError: false,
-      statusCode: 500
+      statusCode: 500,
+      totalFetch: 20,
     }
   }  
 
+  static getInitialProps = ({query}: any) =>{
+    return {query}
+  }
+
   componentDidMount(){
-    axios.get('https://pokeapi.co/api/v2/'+router.query.pageName)
+      this.setState({pageType : this.props.query.pageName})
+      this.getData();
+  }
+
+  getData(){
+    let baseURL = 'https://pokeapi.co/api/v2/'+this.props.query.pageName;
+    if (this.props.query.pageName  == 'pokemon' || this.props.query.pageName == 'ability'){
+      baseURL = baseURL+'?limit='+this.state.totalFetch+'&offset='+this.state.totalFetch
+    }
+    axios.get(baseURL)
     .then((response) => {
       this.setState({pageData: response.data.results, isReady:true})
     }).catch((error) => {
       this.setState({isError : true, statusCode: error.statusCode, isReady:true})
     })
-    .then(function () {
-    });    
+  }
+
+  changeFetch = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    this.setState({totalFetch : parseInt(e.currentTarget.value)})
   }
 
   render() {
@@ -72,8 +93,24 @@ class Home extends React.Component <Props, State> {
           <Header as='h1'>Welcome to Pokémon API</Header>
           <Header.Subheader>{router.query.pageName?.toString().charAt(0).toUpperCase().concat(router.query.pageName?.toString().slice(1).toLowerCase())}</Header.Subheader>
           </Container>
-        
         <br/><br/>
+
+       <Container textAlign='center' style={{}} >
+        { router.query.pageName == 'pokemon' || router.query.pageName == 'ability'
+          ?
+            <>
+            <Form >
+              <Form.Group  inline>
+              <label> Total Data  </label>
+              <Form.Input type='number' value={this.state.totalFetch} onChange={(e)=>this.changeFetch(e)} color='grey'/>
+              <Form.Button content='APPLY'onClick={()=> this.getData()}/>
+              </Form.Group>
+            </Form>
+            <br/><br/>
+            </>
+          : <></>
+        }
+        </Container>
 
         <Card.Group centered textAlign='center' >
         {this.state.pageData?.map((answer, i) => {    
